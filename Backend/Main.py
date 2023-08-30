f8e8eb43caa2978b65f9210c7b1688a46b39c281
whaeundo <1916388@sookmyung.ac.kr>
2023-08-30 23:17:23 +0900

    [Feat] Check word and sentence with whisper

--- Backend/Main.py
+++ Backend/Main.py
@@ -1,6 +1,20 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, File, Query
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.responses import JSONResponse
+
+
+import io
+import asyncio
+# 유사도 분석 import
+import difflib
+
+import whisper
+
 from pydantic import BaseModel
 import openai
 import re
@@ -124,6 +138,133 @@ async def get_paragraph():
     index = 1
     return {"sentence": sentence, "breath": breath, "answerSpeed": answerSpeed, "index": index}
 
+@app.post('/check/sentence')
+async def check_sentence(origin_str: str, file: bytes = File()):
+    with open('audio.mp3', 'wb') as f:
+        f.write(file)
+    
+    # 반환할 값들을 저장하는 딕셔너리
+    res = {}
+    # 숨 쉬는 부분을 나누어 저장하는 리스트
+    breaths = []
+    # 문장을 읽는데 걸리는 시간
+    time = 0
+    # 원본 문장에서 차이가 나는 idx를 저장하는 리스트
+    origin_idx = []
+    # 비교할 문장에서 원본 문장과 차이가 나는 idx를 저장하는 리스트
+    compared_idx = []
+
+    # whisper 음성 파일을 기반으로 STT 처리
+    model = whisper.load_model("base")
+
+    result = model.transcribe("audio.mp3")
+
+    # 연습 단어/문장과 실제로 말한 단어/문장을 비교하여 유사도 계산하기
+    compared_str = result['text'].lstrip()
+
+    res['text'] = compared_str.lstrip()
+
+    # print("result: ", result)
+    # print("compared str: ", compared_str)
+
+    answer_bytes = bytes(origin_str, 'utf-8')
+    input_bytes = bytes(compared_str, 'utf-8')
+    answer_bytes_list = list(answer_bytes)
+    input_bytes_list = list(input_bytes)
+
+    sm = difflib.SequenceMatcher(None, answer_bytes_list, input_bytes_list)
+    similar = sm.ratio()
+
+    for i in result['segments']:
+        breaths.append(i['text'])
+
+    res['breaths'] = breaths
+
+    for i in result['segments']:
+        time += (i['end'] - i['start'])
+
+    res['time'] = time
+
+    # 비교를 위한 Differ 객체 생성
+    differ = difflib.Differ()
+
+    # 두 문장을 비교하고 차이를 출력
+    diff = list(differ.compare(origin_str, compared_str))
+
+    # 차이 출력
+    for idx, line in enumerate(diff):
+        if line[0] != " ":
+            if line[0] == "+":
+                compared_idx.append(idx - len(origin_idx))
+            else:
+                origin_idx.append(idx - len(compared_idx))
+
+    res['similarity'] = similar
+    res['compared_idx'] = compared_idx
+    res['origin_idx'] = origin_idx
+    
+    return JSONResponse(res)
+    
+
+@app.post('/check/word')
+async def check_word(origin_str: str, file: bytes = File()):
+    with open('audio.mp3', 'wb') as f:
+        f.write(file)
+    
+    # 반환할 값들을 저장하는 딕셔너리
+    res = {}
+    # 단어를 읽는데 걸리는 시간
+    time = 0
+    # 원본 단어에서 차이가 나는 idx를 저장하는 리스트
+    origin_idx = []
+    # 비교할 단어에서 원본 단어와 차이가 나는 idx를 저장하는 리스트
+    compared_idx = []
+
+    # whisper 음성 파일을 기반으로 STT 처리
+    model = whisper.load_model("base")
+
+    result = model.transcribe("audio.mp3")
+
+    # 연습 단어/문장과 실제로 말한 단어/문장을 비교하여 유사도 계산하기
+    compared_str = result['text'].lstrip()
+
+    res['text'] = compared_str.lstrip()
+
+    # print("result: ", result)
+    # print("compared str: ", compared_str)
+
+    answer_bytes = bytes(origin_str, 'utf-8')
+    input_bytes = bytes(compared_str, 'utf-8')
+    answer_bytes_list = list(answer_bytes)
+    input_bytes_list = list(input_bytes)
+
+    sm = difflib.SequenceMatcher(None, answer_bytes_list, input_bytes_list)
+    similar = sm.ratio()
+
+    for i in result['segments']:
+        time += (i['end'] - i['start'])
+
+    res['time'] = time
+
+    # 비교를 위한 Differ 객체 생성
+    differ = difflib.Differ()
+
+    # 두 문장을 비교하고 차이를 출력
+    diff = list(differ.compare(origin_str, compared_str))
+
+    # 차이 출력
+    for idx, line in enumerate(diff):
+        if line[0] != " ":
+            if line[0] == "+":
+                compared_idx.append(idx - len(origin_idx))
+            else:
+                origin_idx.append(idx - len(compared_idx))
+
+    res['similarity'] = similar
+    res['compared_idx'] = compared_idx
+    res['origin_idx'] = origin_idx
+    
+    return JSONResponse(res)
 
 
 # TODO: 적정속도 파싱해서 변수에 넣기 온점 물음표 느낌표
