0197d327a03cb1111af6c3222ad2278b85a0a119
김다빈 <mokdb17@gmail.com>
2023-08-30 15:47:54 +0900

    [setting] initial Setting

---
+++ Lingua/Lingua/LinguaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguaApp.swift
+//  Lingua
+//
+//  Created by 김다빈 on 2023/08/30.
+//
+
+import SwiftUI
+
+@main
+struct LinguaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
