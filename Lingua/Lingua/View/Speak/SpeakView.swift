dc6a298cf5d1c82dbe82e0b2fba65e20df8ce20e
김다빈 <mokdb17@gmail.com>
2023-09-01 16:52:00 +0900

    [FEAT] View Animation

--- Lingua/Lingua/View/Speak/SpeakView.swift
+++ Lingua/Lingua/View/Speak/SpeakView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct SpeakView: View {
+    
     @ObservedObject var audioRecorderManger: AudioRecorderManger
     @EnvironmentObject var wordNetwork: NetworkManagerWord
     @EnvironmentObject var sentenceNetwork: NetworkManagerSentence
-    @State private var isAnimate = false
+    @State private var isAnimate = true
     @State private var word = ""
     @State private var sentence = ""
     @State private var selectedOptionIndex = 1
+    @State private var imageIndex = 0
     private let options = ["1단계 단어 말하기", "2단계 문장 말하기", "3단계 문단 말하기"]
-    
+    private let waveImages = ["wave1","wave2","wave3","wave4","wave5"]
     
     var body: some View {
         
@@ -197,7 +199,6 @@ struct SpeakView: View {
                     
                     Button {
                         audioRecorderManger.isRecording ? audioRecorderManger.stopRecording() : audioRecorderManger.startRecording()
-                        isAnimate.toggle()
                     } label: {
                         Image("replay")
                             .resizable()
@@ -206,44 +207,68 @@ struct SpeakView: View {
                     
                     Button {
                         audioRecorderManger.isRecording ? audioRecorderManger.stopRecording() : audioRecorderManger.startRecording()
-                        isAnimate.toggle()
                     } label: {
                         if audioRecorderManger.isRecording == false {
-                            Image("mic")
-                                .resizable()
+                            Circle()
                                 .frame(width:96, height:96)
-                        }
-                        else {
-                            ZStack {
-                                Circle()
-                                    .stroke(Color("Primary").opacity(0.1), lineWidth: 1)
-                                    .foregroundColor(.clear)
-                                    .frame(width: isAnimate ? 96 : 198 ,height: isAnimate ? 96 : 198)
-                                    .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
-                                Circle()
-                                    .stroke(Color("Primary").opacity(0.25), lineWidth: 1)
-                                    .foregroundColor(.clear)
-                                    .frame(width: isAnimate ? 96 : 154 ,height: isAnimate ? 96 : 154)
-                                    .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
-                                Circle()
-                                    .stroke(Color("Primary").opacity(0.5), lineWidth: 1)
-                                    .foregroundColor(.clear)
-                                    .frame(width: isAnimate ? 96 : 118 ,height: isAnimate ? 96 : 118)
-                                    .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
-                                
-                                Image("voice")
-                                    .resizable()
-                                    .foregroundColor(.red)
-                                    .frame(width: 96,height: 96)
-    
-                                    
-    
-                            }
+                                .foregroundColor(Color("Primary"))
+                                .overlay() {
+                                    Image(systemName: "mic")
+                                        .font(.system(size: 30,weight: .bold))
+                                        .foregroundColor(.black)
+                                }
                             
                         }
-                       
-                    }
+                        else {
+                            
+                            Circle()
+                                .foregroundColor(Color("Primary"))
+                                .frame(width: 96,height: 96)
+                                .overlay() {
+                                    Image(waveImages[imageIndex])
+                                        .resizable()
+                                        .frame(width: 70,height: 30)
+                                        .onAppear {
+                                            // 0.5초마다 이미지 인덱스를 증가시키고, 배열 크기를 벗어나면 처음으로 되돌립니다.
+                                            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+                                                withAnimation {
+                                                    if imageIndex < waveImages.count - 1 {
+                                                        imageIndex += 1
+                                                    } else {
+                                                        imageIndex = 0
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    
+                                    Circle()
+                                        .stroke(Color("Primary").opacity(0.1), lineWidth: 1)
+                                        .frame(width: isAnimate ? 96 : 198, height: isAnimate ? 96 : 198)
+                                        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: true))
+                                        .onAppear() {
+                                            isAnimate.toggle()
+                                        }
+                                    
+                                    Circle()
+                                        .stroke(Color("Primary").opacity(0.25), lineWidth: 1)
+                                        .frame(width: isAnimate ? 96 : 154, height: isAnimate ? 96 : 154)
+                                        .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
+                                    
+                                    Circle()
+                                        .stroke(Color("Primary").opacity(0.5), lineWidth: 1)
+                                        .frame(width: isAnimate ? 96 : 118, height: isAnimate ? 96 : 118)
+                                        .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
 
+                                }
+
+                            
+                            
+                        }
+                        
+                        
+                        
+                    }
+                    // TODO: 
                     Button {
                         print("nextPage")
                     } label: {
@@ -251,8 +276,8 @@ struct SpeakView: View {
                             .resizable()
                             .frame(width:48, height:48)
                     }
-
-                  
+                    
+                    
                     
                     
                 }
@@ -283,7 +308,7 @@ struct SpeakView: View {
                     }
                 }
             }
-           
+            
             
         }
         
